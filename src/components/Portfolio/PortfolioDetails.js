b85731e94a3f77bdc451f389d20c6948725b7e70
mohed332 <mohed332@gmail.com>
2023-08-08 15:26:18 +0200

    Committing to heroku

--- src/components/Portfolio/PortfolioDetails.js
+++ src/components/Portfolio/PortfolioDetails.js
@@ -1,5 +1,7 @@
 export const projectsDetails = [
-    {name: "Netflix Clone" , link:"https://netflix-clone-4be74.web.app/" , image:"/projectImages/netflix_clone/netflix-home-2.png" , animation:"fade-right"},
-    {name: "Discord Clone" , link:"https://discord-clone-2021.web.app/" , image:"/projectImages/discord_clone/discord-login.png", animation:"fade-left"},
+    {name: "Netflix Clone" , link:"https://netflix-clone-4be74.web.app/" , image:"./src/images/projectImages/netflix_clone/netflix-home-2.png" , animation:"fade-right"},
+    // {name: "Discord Clone" , link:"https://discord-clone-2021.web.app/" , image:"./../../images/projectImages/discord_clone/discord-login.png", animation:"fade-left"},
+    {name: "Discord Clone" , link:"https://discord-clone-2021.web.app/" , image:"./src/images/projectImages/discord_clone/discord-login.png", animation:"fade-left"},
     // {name: "Covid Tracker" , link:"https://netflix-clone-4be74.web.app/", image:"/projectImages/netflix_clone/netflix-home.png" , animation:"fade-up"}
 ]
+
